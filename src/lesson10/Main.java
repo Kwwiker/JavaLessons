43da928959f3c474150f9f0be352714b58da5505
Kirill Pustovetov <46481837+Kwwiker@users.noreply.github.com>
2021-11-08 15:16:23 +0300

    update name

--- src/lesson10/Main.java
+++ src/lesson10/Main.java
@@ -2,9 +2,9 @@ package lesson10;
 
 public class Main {
     public static void main(String[] args) {
-        Object LOCK_OBJECT = new Object();
-        MyThread ping = new MyThread(LOCK_OBJECT, "Ping!");
-        MyThread pong = new MyThread(LOCK_OBJECT, "Pong!");
+        Object LOCK = new Object();
+        MyThread ping = new MyThread(LOCK, "Ping!");
+        MyThread pong = new MyThread(LOCK, "Pong!");
         ping.start();
         pong.start();
     }
